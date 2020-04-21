cf0439ec4a06764f0745e59e1cb929d13dc013cb
securibee <securibee@gmail.com>
2020-04-21 14:13:44 -0400

    style: add basic styling

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,11 +1,23 @@
 <template>
-  <article>
-    <header>
-      <h2>{{ post.title }}</h2>
-      <h3>{{ post.date }}</h3>
-      <img :src="post.cover_image" :alt="post.title" />
+  <article class="container mx-auto">
+    <header class="text-center pt-16 md:pt-32">
+      <div class="flex justify-center text-sm md:text-base font-medium">
+        <span>{{ date }}</span>
+        <div v-if="post.tag_list">
+          <span class="text-gray-900 px-2">/</span>{{ post.tag_list[0] }}
+        </div>
+      </div>
+      <h1
+        class="bg-yellow-400 px-6 my-4 inline-block text-center font-bold break-normal text-3xl md:text-5xl"
+      >
+        {{ post.title }}
+      </h1>
     </header>
-    <section v-html="$md.render(post.body_markdown)"></section>
+    <img :src="post.cover_image" :alt="post.title" />
+    <section
+      class="max-w-3xl z-10 relative mx-auto -mt-10 bg-white p-8 post md:p-24"
+      v-html="$md.render(post.body_markdown)"
+    />
   </article>
 </template>
 
@@ -23,6 +35,13 @@ export default {
     }
   },
 
+  computed: {
+    date() {
+      const date = new Date(this.post.published_at)
+      return date.toDateString()
+    }
+  },
+
   data() {
     return {
       post: {}
@@ -31,4 +50,8 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="postcss" scoped>
+.post {
+  @apply text-2xl;
+}
+</style>
