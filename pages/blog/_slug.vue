b1145aa9c2849825e4d5e5783b97e92e62fc9f46
securibee <securibee@gmail.com>
2020-04-22 08:47:29 -0400

    feat: add navigation and style single post

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -1,31 +1,38 @@
 <template>
-  <article class="container mx-auto">
-    <header class="text-center pt-16 md:pt-32">
-      <div class="flex justify-center text-sm md:text-base font-medium">
-        <span>{{ date }}</span>
-        <div v-if="post.tag_list">
-          <span class="text-gray-900 px-2">/</span>{{ post.tag_list[0] }}
+  <div>
+    <BlogNavigation />
+    <article class="container mx-auto">
+      <header class="text-center pt-16 md:pt-32">
+        <div class="flex justify-center text-sm md:text-base font-medium">
+          <span>{{ date }}</span>
+          <div v-if="post.tag_list">
+            <span class="text-gray-900 px-2">/</span>{{ post.tag_list[0] }}
+          </div>
         </div>
-      </div>
-      <h1
-        class="bg-yellow-400 px-6 my-4 inline-block text-center font-bold break-normal text-3xl md:text-5xl"
-      >
-        {{ post.title }}
-      </h1>
-    </header>
-    <img :src="post.cover_image" :alt="post.title" />
-    <section
-      class="max-w-3xl z-10 relative mx-auto -mt-10 bg-white p-8 post md:p-24"
-      v-html="$md.render(post.body_markdown)"
-    />
-  </article>
+        <h1
+          class="bg-yellow-400 px-6 my-4 inline-block text-center font-bold break-normal text-3xl md:text-5xl"
+        >
+          {{ post.title }}
+        </h1>
+      </header>
+      <img :src="post.cover_image" :alt="post.title" />
+      <section
+        class="max-w-3xl z-10 relative mx-auto -mt-10 bg-white p-8 post md:p-24"
+        v-html="$md.render(post.body_markdown)"
+      />
+    </article>
+  </div>
 </template>
 
 <script>
 const getDevtoPosts = () =>
   import('~/data/devto.json').then((m) => m.default || m)
+const BlogNavigation = () => import('@/components/BlogNavigation')
 
 export default {
+  components: {
+    BlogNavigation
+  },
   async asyncData({ params }) {
     const posts = await getDevtoPosts()
     const post = posts.find((p) => p.slug === params.slug)
@@ -35,16 +42,16 @@ export default {
     }
   },
 
+  data() {
+    return {
+      post: {}
+    }
+  },
+
   computed: {
     date() {
       const date = new Date(this.post.published_at)
       return date.toDateString()
-    }
-  },
-
-  data() {
-    return {
-      post: {}
     }
   }
 }
